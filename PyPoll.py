b32ac0aa9958c1d8d401a5685cf84f278e34bbc7
brendon sprague <nytech42@gmail.com>
2022-07-27 14:11:09 -0600

    removed date time module from pypoll

--- PyPoll.py
+++ PyPoll.py
@@ -5,15 +5,6 @@
 # 4. The total number of votes each candidate won.
 # 5. The winner of the election based on popular vote.
 
-#Import the datetime class from the datetime module.
-import datetime as dt
-
-# Use the now() attribute on the datetime class to get the present time.
-now=dt.datetime.now() 
-
-# Print the present time
-print("the time right now is",now)
-
 
 import csv
 
